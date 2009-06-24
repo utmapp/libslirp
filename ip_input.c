c825316027e32a0f035c1f9c521dd51320856188
Jan Kiszka <jan.kiszka@siemens.com>
2009-06-24 14:42:30 +0200

    slirp: Make IP packet ID consistent
    Currently, ip_id is always initialized to 0 on slirp startup (despite
    the broken attempt to derive it from the clock). This is good for
    reproducibility. But it is not preserved across save/restore. This patch
    therefore drops the dead initialization code from ip_init and introduces
    ip_id to the persistent slirp state.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- ip_input.c
+++ ip_input.c
@@ -56,7 +56,6 @@ static void ip_deq(register struct ipasfrag *p);
 void ip_init(void)
 {
     ipq.ip_link.next = ipq.ip_link.prev = &ipq.ip_link;
-    ip_id = tt.tv_sec & 0xffff;
     udp_init();
     tcp_init();
 }
