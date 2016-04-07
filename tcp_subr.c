804f441a9d6998a57040bf36685a17a6436b2ea8
Steven Luo <steven+qemu@steven676.net>
2016-04-06 22:04:21 -0700

    slirp: don't crash when tcp_sockclosed() is called with a NULL tp
    Signed-off-by: Steven Luo <steven+qemu@steven676.net>
    Reviewed-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>
    Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>

--- tcp_subr.c
+++ tcp_subr.c
@@ -355,6 +355,10 @@ void tcp_sockclosed(struct tcpcb *tp)
     DEBUG_CALL("tcp_sockclosed");
     DEBUG_ARG("tp = %p", tp);
 
+    if (!tp) {
+        return;
+    }
+
     switch (tp->t_state) {
     case TCPS_CLOSED:
     case TCPS_LISTEN:
@@ -372,8 +376,7 @@ void tcp_sockclosed(struct tcpcb *tp)
         tp->t_state = TCPS_LAST_ACK;
         break;
     }
-    if (tp)
-        tcp_output(tp);
+    tcp_output(tp);
 }
 
 /*
