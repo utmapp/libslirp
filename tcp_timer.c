db733e20e0f8da46bfbed45f56a0ee2f779eda1c
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-11-14 16:36:28 +0400

    slirp: replace compile time DO_KEEPALIVE
    Use a global variable instead (similar to slirp_debug)
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>

--- tcp_timer.c
+++ tcp_timer.c
@@ -255,7 +255,7 @@ static struct tcpcb *tcp_timers(register struct tcpcb *tp, int timer)
         if (tp->t_state < TCPS_ESTABLISHED)
             goto dropit;
 
-        if ((SO_OPTIONS) && tp->t_state <= TCPS_CLOSE_WAIT) {
+        if (slirp_do_keepalive && tp->t_state <= TCPS_CLOSE_WAIT) {
             if (tp->t_idle >= TCPTV_KEEP_IDLE + TCP_MAXIDLE)
                 goto dropit;
             /*
