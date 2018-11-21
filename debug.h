c4d1946d24fc0436fb72fa8e369aff5aacd05503
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-11-21 23:40:34 +0100

    slirp: Mark debugging calls as unlikely
    to get them out of the hot path.
    
    Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- debug.h
+++ debug.h
@@ -14,32 +14,32 @@
 
 extern int slirp_debug;
 
-#define DEBUG_CALL(fmt, ...)                   \
-    do {                                       \
-        if (slirp_debug & DBG_CALL) {          \
-            g_debug(fmt "...", ##__VA_ARGS__); \
-        }                                      \
+#define DEBUG_CALL(fmt, ...)                      \
+    do {                                          \
+        if (G_UNLIKELY(slirp_debug & DBG_CALL)) { \
+            g_debug(fmt "...", ##__VA_ARGS__);    \
+        }                                         \
     } while (0)
 
-#define DEBUG_ARG(fmt, ...)                  \
-    do {                                     \
-        if (slirp_debug & DBG_CALL) {        \
-            g_debug(" " fmt, ##__VA_ARGS__); \
-        }                                    \
+#define DEBUG_ARG(fmt, ...)                       \
+    do {                                          \
+        if (G_UNLIKELY(slirp_debug & DBG_CALL)) { \
+            g_debug(" " fmt, ##__VA_ARGS__);      \
+        }                                         \
     } while (0)
 
-#define DEBUG_MISC(fmt, ...)             \
-    do {                                 \
-        if (slirp_debug & DBG_MISC) {    \
-            g_debug(fmt, ##__VA_ARGS__); \
-        }                                \
+#define DEBUG_MISC(fmt, ...)                      \
+    do {                                          \
+        if (G_UNLIKELY(slirp_debug & DBG_MISC)) { \
+            g_debug(fmt, ##__VA_ARGS__);          \
+        }                                         \
     } while (0)
 
-#define DEBUG_ERROR(fmt, ...)            \
-    do {                                 \
-        if (slirp_debug & DBG_ERROR) {   \
-            g_debug(fmt, ##__VA_ARGS__); \
-        }                                \
+#define DEBUG_ERROR(fmt, ...)                      \
+    do {                                           \
+        if (G_UNLIKELY(slirp_debug & DBG_ERROR)) { \
+            g_debug(fmt, ##__VA_ARGS__);           \
+        }                                          \
     } while (0)
 
 #endif /* DEBUG_H_ */
