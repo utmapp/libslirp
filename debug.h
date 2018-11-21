17b6500e8f719de543d3340254731c0a77f13945
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-11-22 02:06:40 +0400

    slirp: replace DEBUG_ARGS with DEBUG_ARG
    There is no clear benefit in calling an alias DEBUG_ARGS(). Replace
    calls with DEBUG_ARG(), and fix the white-spacing while at it.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>

--- debug.h
+++ debug.h
@@ -35,8 +35,6 @@ extern int slirp_debug;
         }                                     \
     } while (0)
 
-#define DEBUG_ARGS(fmt, ...) DEBUG_ARG(fmt, ##__VA_ARGS__)
-
 #define DEBUG_MISC(fmt, ...)                  \
     do {                                      \
         if (slirp_debug & DBG_MISC) {         \
