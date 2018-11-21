c5844935f15692b5fd12aae1a6704336265b6488
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-11-22 02:06:39 +0400

    slirp: remove remaining DEBUG blocks
    Instead, rely on slirp_debug flags, or compile unconditionally (the
    substraction in cksum is unlikely to affect any benchmark result).
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>

--- ip_icmp.c
+++ ip_icmp.c
@@ -259,14 +259,12 @@ void icmp_send_error(struct mbuf *msrc, u_char type, u_char code, int minsize,
     if (!msrc)
         goto end_error;
     ip = mtod(msrc, struct ip *);
-#ifdef DEBUG
-    {
+    if (slirp_debug & DBG_MISC) {
         char bufa[20], bufb[20];
         strcpy(bufa, inet_ntoa(ip->ip_src));
         strcpy(bufb, inet_ntoa(ip->ip_dst));
         DEBUG_MISC(" %.16s to %.16s\n", bufa, bufb);
     }
-#endif
     if (ip->ip_off & IP_OFFMASK)
         goto end_error; /* Only reply to fragment 0 */
 
