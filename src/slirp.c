utmapp/libslirp

commit 09d410adbff5422b7ba7596bce0ca71f9f807ea9

allow custom MTU
From https://github.com/rootless-containers/slirp4netns/blob/4889f5299f407d7d7566c76a3b8b5f71c99b6db5/qemu_patches/0002-slirp-allow-custom-MTU.patch

Original commits:
* https://github.com/rootless-containers/slirp4netns/commit/ea630a7e945cf538184ff1b1b4bd7b8ddc01993e
* https://github.com/rootless-containers/slirp4netns/commit/1508a66c93c223555f08651592dde3d2d708b166
* https://github.com/rootless-containers/slirp4netns/commit/19f3f41df4066d6103e6f882500e24db7ea7d9e1
* https://github.com/rootless-containers/slirp4netns/commit/a11abedafcc627ef0657999e63b211b0f26d4c02
* https://github.com/rootless-containers/slirp4netns/commit/2adbd7c449944d3b837164c86eedd3dcabbba1a6

Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

src/slirp.c
@@ -271,6 +271,10 @@ Slirp *slirp_new(const SlirpConfig *cfg, const SlirpCb *callbacks, void *opaque)
 {
     Slirp *slirp;
     g_return_val_if_fail(cfg != NULL, NULL);
+    g_return_val_if_fail(cfg->if_mtu >= IF_MTU_MIN || cfg->if_mtu == 0, NULL);
+    g_return_val_if_fail(cfg->if_mtu <= IF_MTU_MAX, NULL);
+    g_return_val_if_fail(cfg->if_mru >= IF_MRU_MIN || cfg->if_mru == 0, NULL);
+    g_return_val_if_fail(cfg->if_mru <= IF_MRU_MAX, NULL);
 
     slirp = g_malloc0(sizeof(Slirp));
 
@@ -311,6 +315,8 @@ Slirp *slirp_new(const SlirpConfig *cfg, const SlirpCb *callbacks, void *opaque)
     if (cfg->vdnssearch) {
         translate_dnssearch(slirp, cfg->vdnssearch);
     }
+    slirp->if_mtu = cfg->if_mtu == 0 ? IF_MTU_DEFAULT : cfg->if_mtu;
+    slirp->if_mru = cfg->if_mru == 0 ? IF_MRU_DEFAULT : cfg->if_mru;
 
     return slirp;
 }
@@ -917,7 +923,7 @@ static int if_encap6(Slirp *slirp, struct mbuf *ifm, struct ethhdr *eh,
  */
 int if_encap(Slirp *slirp, struct mbuf *ifm)
 {
-    uint8_t buf[1600];
+    uint8_t buf[IF_MTU_MAX + 100];
     struct ethhdr *eh = (struct ethhdr *)buf;
     uint8_t ethaddr[ETH_ALEN];
     const struct ip *iph = (const struct ip *)ifm->m_data;

src/dhcpv6.c
@@ -178,7 +178,7 @@ static void dhcpv6_info_request(Slirp *slirp, struct sockaddr_in6 *srcsas,
 
         *resp++ = OPTION_BOOTFILE_URL >> 8; /* option-code high byte */
         *resp++ = OPTION_BOOTFILE_URL; /* option-code low byte */
-        smaxlen = (uint8_t *)m->m_data + IF_MTU - (resp + 2);
+        smaxlen = (uint8_t *)m->m_data + slirp->if_mtu - (resp + 2);
         slen = snprintf((char *)resp + 2, smaxlen,
                         "tftp://[%02x%02x:%02x%02x:%02x%02x:%02x%02x:"
                         "%02x%02x:%02x%02x:%02x%02x:%02x%02x]/%s",

src/if.h
@@ -11,8 +11,12 @@
 #define IF_AUTOCOMP 0x04 /* Autodetect (default) */
 #define IF_NOCIDCOMP 0x08 /* CID compression */
 
-#define IF_MTU 1500
-#define IF_MRU 1500
+#define IF_MTU_DEFAULT 1500
+#define IF_MTU_MIN 68
+#define IF_MTU_MAX 65521
+#define IF_MRU_DEFAULT 1500
+#define IF_MRU_MIN 68
+#define IF_MRU_MAX 65521
 #define IF_COMP IF_AUTOCOMP /* Flags for compression */
 
 /* 2 for alignment, 14 for ethernet */

src/ip6_icmp.c
@@ -94,8 +94,8 @@ void icmp6_send_error(struct mbuf *m, uint8_t type, uint8_t code)
     DEBUG_ARG("target = %s", addrstr);
 
     rip->ip_nh = IPPROTO_ICMPV6;
-    const int error_data_len =
-        MIN(m->m_len, IF_MTU - (sizeof(struct ip6) + ICMP6_ERROR_MINLEN));
+    const int error_data_len = MIN(
+        m->m_len, slirp->if_mtu - (sizeof(struct ip6) + ICMP6_ERROR_MINLEN));
     rip->ip_pl = htons(ICMP6_ERROR_MINLEN + error_data_len);
     t->m_len = sizeof(struct ip6) + ntohs(rip->ip_pl);
 
@@ -112,7 +112,7 @@ void icmp6_send_error(struct mbuf *m, uint8_t type, uint8_t code)
         ricmp->icmp6_err.unused = 0;
         break;
     case ICMP6_TOOBIG:
-        ricmp->icmp6_err.mtu = htonl(IF_MTU);
+        ricmp->icmp6_err.mtu = htonl(slirp->if_mtu);
         break;
     case ICMP6_PARAMPROB:
         /* TODO: Handle this case */

src/ip6_input.c
@@ -44,7 +44,7 @@ void ip6_input(struct mbuf *m)
         goto bad;
     }
 
-    if (ntohs(ip6->ip_pl) > IF_MTU) {
+    if (ntohs(ip6->ip_pl) > slirp->if_mtu) {
         icmp6_send_error(m, ICMP6_TOOBIG, 0);
         goto bad;
     }

src/ip_output.c
@@ -72,7 +72,7 @@ int ip_output(struct socket *so, struct mbuf *m0)
     /*
      * If small enough for interface, can just send directly.
      */
-    if ((uint16_t)ip->ip_len <= IF_MTU) {
+    if ((uint16_t)ip->ip_len <= slirp->if_mtu) {
         ip->ip_len = htons((uint16_t)ip->ip_len);
         ip->ip_off = htons((uint16_t)ip->ip_off);
         ip->ip_sum = 0;
@@ -91,7 +91,7 @@ int ip_output(struct socket *so, struct mbuf *m0)
         goto bad;
     }
 
-    len = (IF_MTU - hlen) & ~7; /* ip databytes per packet */
+    len = (slirp->if_mtu - hlen) & ~7; /* ip databytes per packet */
     if (len < 8) {
         error = -1;
         goto bad;

src/libslirp.h
@@ -85,6 +85,10 @@ typedef struct SlirpConfig {
     struct in6_addr vnameserver6;
     const char **vdnssearch;
     const char *vdomainname;
+    /* Default: IF_MTU_DEFAULT */
+    size_t if_mtu;
+    /* Default: IF_MRU_DEFAULT */
+    size_t if_mru;
 } SlirpConfig;
 
 Slirp *slirp_new(const SlirpConfig *cfg, const SlirpCb *callbacks,

src/mbuf.c
@@ -20,8 +20,8 @@
 /*
  * Find a nice value for msize
  */
-#define SLIRP_MSIZE \
-    (offsetof(struct mbuf, m_dat) + IF_MAXLINKHDR + TCPIPHDR_DELTA + IF_MTU)
+#define SLIRP_MSIZE(mtu) \
+    (offsetof(struct mbuf, m_dat) + IF_MAXLINKHDR + TCPIPHDR_DELTA + (mtu))
 
 void m_init(Slirp *slirp)
 {
@@ -66,7 +66,7 @@ struct mbuf *m_get(Slirp *slirp)
     DEBUG_CALL("m_get");
 
     if (slirp->m_freelist.qh_link == &slirp->m_freelist) {
-        m = g_malloc(SLIRP_MSIZE);
+        m = g_malloc(SLIRP_MSIZE(slirp->if_mtu));
         slirp->mbuf_alloced++;
         if (slirp->mbuf_alloced > MBUF_THRESH)
             flags = M_DOFREE;
@@ -81,7 +81,7 @@ struct mbuf *m_get(Slirp *slirp)
     m->m_flags = (flags | M_USEDLIST);
 
     /* Initialise it */
-    m->m_size = SLIRP_MSIZE - offsetof(struct mbuf, m_dat);
+    m->m_size = SLIRP_MSIZE(slirp->if_mtu) - offsetof(struct mbuf, m_dat);
     m->m_data = m->m_dat;
     m->m_len = 0;
     m->m_nextpkt = NULL;

commit c6cc0eb1ebd55cb9b35107b3aa87a4680c47f3ca

build-sys: fix NetBSD build regression
Fixes: 09d410adbff5422b7ba7596bce0ca71f9f807ea9 ("allow custom MTU")

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

src/slirp.c
@@ -29,6 +29,11 @@
 #include <net/if.h>
 #endif
 
+/* https://gitlab.freedesktop.org/slirp/libslirp/issues/18 */
+#if defined(__NetBSD__) && defined(if_mtu)
+#undef if_mtu
+#endif
+
 int slirp_debug;
 
 /* Define to 1 if you want KEEPALIVE timers */