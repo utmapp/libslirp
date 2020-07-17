eff957d5b04578573fa6bc6e851e950da80ba51b
Dr. David Alan Gilbert <dgilbert@redhat.com>
2020-07-17 18:17:41 +0100

    ip_stripoptions use memmove
    ip_stripoptions is moving data long in the same buffer; that's undefined
    with memcpy, use memmove.
    
    Buglink: https://bugs.launchpad.net/qemu/+bug/1878043
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- src/ip_input.c
+++ src/ip_input.c
@@ -454,7 +454,7 @@ void ip_stripoptions(register struct mbuf *m, struct mbuf *mopt)
     olen = (ip->ip_hl << 2) - sizeof(struct ip);
     opts = (char *)(ip + 1);
     i = m->m_len - (sizeof(struct ip) + olen);
-    memcpy(opts, opts + olen, (unsigned)i);
+    memmove(opts, opts + olen, (unsigned)i);
     m->m_len -= olen;
 
     ip->ip_hl = sizeof(struct ip) >> 2;
